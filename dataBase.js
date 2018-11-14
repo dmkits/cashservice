fb8c132d7bb576053931058307461f462d042453
dmkits <dmk.dp.ua@gmail.com>
2018-11-14 15:21:03 +0200

    fix bug on dataBase fillToSalePays.

--- dataBase.js
+++ dataBase.js
@@ -149,15 +149,15 @@ module.exports.fillToSalePays = function (CHID, cheque, callback) {
                     callback(err);
                     return;
                 }
+                addToSalePays(CHID, cheque, function (err, res) {
+                    if (err) {
+                        callback(err);
+                        return;
+                    }
+                    callback(null,outData);
+                })
             });
         }
-        addToSalePays(CHID, cheque, function (err, res) {
-            if (err) {
-                callback(err);
-                return;
-            }
-            callback(null,outData);
-        })
     })
 };
 
