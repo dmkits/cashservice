fb8d5c98f7c83bd2e2f0bbab242686204a88639b
dmkits <dmk.dp.ua@gmail.com>
2019-03-06 13:46:52 +0200

    Fix bugs in scripts ins_MonIntExp, ins_MonIntRec.

--- scripts/ins_MonIntRec.sql
+++ scripts/ins_MonIntRec.sql
@@ -1,20 +1,17 @@
 declare @NewChID INT
 exec dbo.z_NewChID 't_MonIntRec', @NewChID OUTPUT
-
-declare @DocID INT
-exec dbo.z_NewDocID 11051,'t_MonIntRec', @OurID, @DocID OUTPUT
-
 declare  @CRID INT, @OurID INT
 select  @CRID=c.CRID, @OurID = r.OurID
 from r_Crs c, r_CRSrvs r WHERE r.SrvID =c.SrvID AND c.FacID=@FacID
+declare @DocID INT
+exec dbo.z_NewDocID 11051,'t_MonIntRec', @OurID, @DocID OUTPUT
 declare @OperID INT
 select @OperID=OperID from r_OperCrs WHERE CRID=@CRID AND CROperID = @CROperID
-
 INSERT INTO t_MonIntRec (ChID,	OurID,	CRID,	DocDate,	SumCC,
-                        Notes,	OperID,	DocTime,	CodeID1,	CodeID2,
-                        CodeID3,	CodeID4,	CodeID5,	StateCode,	DocID,
-                        IntDocID)
-               VALUES  (@NewChID,@OurID,@CRID, @DocDate,@SumCC,
-                        '',@OperID,@DocTime, 0,0,
-                        0,0,0,0,@DocID,
-                        @DocID)
+  Notes,	OperID,	DocTime,	CodeID1,	CodeID2,
+  CodeID3,	CodeID4,	CodeID5,	StateCode,	DocID,
+  IntDocID)
+VALUES  (@NewChID,@OurID,@CRID, @DocDate,@SumCC,
+  '',@OperID,@DocTime, 0,0,
+  0,0,0,0,@DocID,
+  @DocID)
--- scripts/ins_ZRep.sql
+++ scripts/ins_ZRep.sql
@@ -10,23 +10,22 @@ declare @DocID INT
 exec dbo.z_NewDocID 11951,'t_zRep', @OurID, @DocID OUTPUT
 
 INSERT INTO t_zRep  (ChID, DocDate,	DocTime,	CRID,	OperID,
-                    OurID,	DocID,	FacID,	FinID,	ZRepNum,
-                    SumCC_wt,	Sum_A,	Sum_B,	Sum_C,	Sum_D,
-                    RetSum_A,	RetSum_B,RetSum_C,	RetSum_D,	SumCash,
-                    SumCard,	SumCredit, SumCheque,	SumOther,	RetSumCash,
-                    RetSumCard,	RetSumCredit, RetSumCheque,	RetSumOther,	SumMonRec,
-                    SumMonExp,	SumRem, Notes,	Sum_E,	Sum_F,	RetSum_E,
-                    RetSum_F,	Tax_A,	Tax_B,
-                    Tax_C,	Tax_D,	Tax_E,	Tax_F, 	RetTax_A,	RetTax_B,	RetTax_C,
-                    RetTax_D,	RetTax_E,	RetTax_F)
-
-          VALUES   (@NewChID, @DocDate,	@DocTime,	@CRID,	@OperID,
-                    @OurID,	@DocID,	@FacID,	@FinID,	@ZRepNum,
-                    @SumCC_wt,	@Sum_A,	@Sum_B,	@Sum_C,	@Sum_D,
-                    0,	0, 0,	0,	@SumCash,
-                    @SumCard,	0, 0,	0,	0,
-                    0,	0, 0,	0,	@SumMonRec,
-                    @SumMonExp,	0, '',	@Sum_E,	0,	0,
-                    0,	@Tax_A,	@Tax_B,
-                    @Tax_C,	@Tax_D, @Tax_E,	@Tax_F,	0,	0,	0,
-                    0,	0,	0)
+  OurID,	DocID,	FacID,	FinID,	ZRepNum,
+  SumCC_wt,	Sum_A,	Sum_B,	Sum_C,	Sum_D,
+  RetSum_A,	RetSum_B,RetSum_C,	RetSum_D,	SumCash,
+  SumCard,	SumCredit, SumCheque,	SumOther,	RetSumCash,
+  RetSumCard,	RetSumCredit, RetSumCheque,	RetSumOther,	SumMonRec,
+  SumMonExp,	SumRem, Notes,	Sum_E,	Sum_F,	RetSum_E,
+  RetSum_F,	Tax_A,	Tax_B,
+  Tax_C,	Tax_D,	Tax_E,	Tax_F, 	RetTax_A,	RetTax_B,	RetTax_C,
+  RetTax_D,	RetTax_E,	RetTax_F)
+VALUES(@NewChID, @DocDate,	@DocTime,	@CRID,	@OperID,
+  @OurID,	@DocID,	@FacID,	@FinID,	@ZRepNum,
+  @SumCC_wt,	@Sum_A,	@Sum_B,	@Sum_C,	@Sum_D,
+  0,	0, 0,	0,	@SumCash,
+  @SumCard,	0, 0,	0,	0,
+  0,	0, 0,	0,	@SumMonRec,
+  @SumMonExp,	0, '',	@Sum_E,	0,	0,
+  0,	@Tax_A,	@Tax_B,
+  @Tax_C,	@Tax_D, @Tax_E,	@Tax_F,	0,	0,	0,
+  0,	0,	0)
